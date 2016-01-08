770ae249eb97e4abb3b1d575004bd7f3259c8255
Yago <yago.ftw@gmail.com>
2016-01-09 00:53:14 +0100

    Aditional params in launch.sh

--- launch.sh
+++ launch.sh
@@ -3,4 +3,4 @@ THIS_DIR=$(cd $(dirname $0); pwd)
 
 export TELEGRAM_HOME=$THIS_DIR
 
-./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./main.lua -l 1 -E
+./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./main.lua -l 1 -E $@
